e8054feba14c70f56b93bf581149f1ee90a928fe
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2022-04-24 19:31:13 +0530

    Note taker

--- Sboot_TODO_NoteTakerApp/src/main/java/todo/dao/NoteRepo.java
+++ Sboot_TODO_NoteTakerApp/src/main/java/todo/dao/NoteRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import todo.entity.Note;
 
@@ -17,7 +18,7 @@ public interface NoteRepo extends JpaRepository<Note, Integer> {
 
 	// case-in-sensitive searching
 	@Query("select n from Note n where lower(n.content) like concat('%', :keyword,'%')")
-	public List<Note> getNotesContainingKeyword(String keyword);
+	public List<Note> getNotesContainingKeyword(@Param("keyword") String keyword);
 }
 
 
