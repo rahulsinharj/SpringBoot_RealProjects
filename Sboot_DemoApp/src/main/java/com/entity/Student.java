12c2b7b0b0d4f982bba63c14703603dc623ac25d
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-08-19 21:01:38 +0530

    DemoApp

--- Sboot_DemoApp/src/main/java/com/entity/Student.java
+++ Sboot_DemoApp/src/main/java/com/entity/Student.java
@@ -6,11 +6,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "student_demo_details")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
 	@Id
