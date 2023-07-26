e2ee6e7c392f1f4a8945a2e9091752de56e2ccc4
rahulsinharj <rahulkumarsinha1234@gmail.com>
2023-07-26 22:29:57 +0530

    Spring

--- Sboot_DemoApp_Student/src/main/java/com/entity/Student.java
+++ Sboot_DemoApp_Student/src/main/java/com/entity/Student.java
@@ -13,8 +13,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "student_demo_details")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Student {
 
 	@Id
@@ -30,6 +28,55 @@ public class Student {
 	private String bloodgroup;
 	
 	private double salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getBloodgroup() {
+		return bloodgroup;
+	}
+
+	public void setBloodgroup(String bloodgroup) {
+		this.bloodgroup = bloodgroup;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
 	
 	
 	
